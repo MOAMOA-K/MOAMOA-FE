cb978a2f9d5622010c1f0b58f55a69a0fcbadea4
Injun Jeong <jijun897@naver.com>
2025-08-14 21:14:24 +0900

    feat: 라우터 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalResetStyle } from '@/styles/reset';
 import { GlobalTypographyStyle } from '@/styles/typography';
 import { theme } from './styles/theme';
+import Router from '@/routes/router';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalResetStyle} />
       <Global styles={GlobalTypographyStyle} />
+      <Router />
     </ThemeProvider>
   );
 }
