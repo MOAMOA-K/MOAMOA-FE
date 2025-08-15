88ab3767d8618883e1c90492b2d413b249686cf0
Junsub Lee <lee980605@knu.ac.kr>
2025-08-16 02:39:43 +0900

    feat: MAP 경로 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router';
 import { ROUTE_PATH } from './paths';
 import MainPage from '@/pages/main/MainPage';
+import MapPage from '@/pages/main/MapPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path={ROUTE_PATH.HOME} element={<MainPage />} />
+      <Route path={ROUTE_PATH.MAP} element={<MapPage />} />
     </Routes>
   );
 };
