ff32927ba3cbfa2365da24078960ef5638fb8ce9
Injun Jeong <jijun897@naver.com>
2025-08-19 23:09:53 +0900

    feat: 내 정보 페이지 로그아웃 버튼 추가

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -4,14 +4,25 @@ import NavigationCustomer from '@/components/layout/NavigationCustomer';
 import { HEADER_HEIGHT, NAV_HEIGHT } from '@/constants/number';
 import StatusSection from './components/StatusSection';
 import HistorySection from './components/HistorySection';
+import Typography from '@/components/UI/Typography';
+import { useTheme } from '@emotion/react';
 
 const MyPage = () => {
+  const theme = useTheme();
+
   return (
     <>
       <Header title='마이페이지' />
       <Main>
         <StatusSection />
         <HistorySection />
+        <LogoutSection>
+          <LogoutButton>
+            <Typography variant='body2' color={theme.colors.gray[0]}>
+              로그아웃
+            </Typography>
+          </LogoutButton>
+        </LogoutSection>
       </Main>
       <NavigationCustomer />
     </>
@@ -24,4 +35,26 @@ const Main = styled.main`
   height: 100%;
   min-height: calc(100dvh - ${HEADER_HEIGHT}px - ${NAV_HEIGHT}px);
   background-color: #fcfcfc;
+`;
+
+const LogoutSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: ${({ theme }) => theme.spacing[6]};
+`;
+
+const LogoutButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 450px;
+  padding: ${({ theme }) => `${theme.spacing[3]} ${theme.spacing[4]}`};
+  background-color: ${({ theme }) => theme.colors.customer.main};
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  box-sizing: border-box;
 `;
