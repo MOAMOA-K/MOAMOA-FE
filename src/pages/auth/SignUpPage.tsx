44d8ba6c336c8a316f4f41c8a8a8214ed8512f8c
Jeong In Jun <jijun897@naver.com>
2025-08-23 21:58:00 +0900

    feat: 회원가입 페이지 퍼블리싱

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,7 +1,56 @@
+import Typography from '@/components/UI/Typography';
+import { ROUTE_PATH } from '@/routes/paths';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import FormItem from './components/FormItem';
+import FormRadioItem from './components/FormRadioItem';
+import { useState } from 'react';
 
 const SignUpPage = () => {
-  return <Main></Main>;
+  const [role, setRole] = useState<string>('ROLE_CUSTOMER');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(role);
+    // Handle form submission
+  };
+
+  return (
+    <Main>
+      <Typography variant='title1' weight='bold' as='h1'>
+        모아모아뀽
+      </Typography>
+      <Form onSubmit={handleSubmit}>
+        <FormItem
+          label='닉네임'
+          type='text'
+          placeholder='닉네임을 입력해주세요'
+        />
+        <FormItem
+          label='이메일'
+          type='email'
+          placeholder='이메일을 입력해주세요'
+        />
+        <FormItem
+          label='비밀번호'
+          type='password'
+          placeholder='비밀번호를 입력해주세요'
+        />
+        <FormRadioItem role={role} setRole={setRole} />
+        <Button type='submit'>
+          <Typography variant='subtitle1' weight='bold' color='white'>
+            회원가입
+          </Typography>
+        </Button>
+      </Form>
+
+      <Link to={ROUTE_PATH.LOGIN}>
+        <Typography variant='body1' color='sub'>
+          아이디가 이미 있으신가요? 로그인하기
+        </Typography>
+      </Link>
+    </Main>
+  );
 };
 
 export default SignUpPage;
@@ -10,4 +59,30 @@ const Main = styled.main`
   height: 100%;
   min-height: 100dvh;
   background-color: ${({ theme }) => theme.colors.default.background};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing[4]};
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 450px;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[4]};
+  padding: ${({ theme }) => theme.spacing[6]};
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.customer.main};
+  border: none;
+  border-radius: 20px;
+  margin-top: ${({ theme }) => theme.spacing[2]};
+  padding: ${({ theme }) => theme.spacing[2]} ${({ theme }) => theme.spacing[4]};
+  cursor: pointer;
 `;
