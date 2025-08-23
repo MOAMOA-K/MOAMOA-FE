fffb44f9622d837b2fd9bb842f7528064e53d972
Junsub Lee <lee980605@knu.ac.kr>
2025-08-24 04:37:09 +0900

    feat: API 경로 추가

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -2,9 +2,9 @@ export const API_PATHS = {
   LOGIN: '/api/auth/login',
   LOGOUT: '/api/auth/logout',
   SIGNUP: '/api/auth/signup',
+  FEEDBACK: `/api/feedbacks`,
+  ANNOUNCEMENT: `/api/announcement`,
 
   STORE_LIST: '/api/store/list',
   STORE_DETAIL: (id: string | number) => `/api/store/${id}`,
-  FEEDBACK: `/api/feedbacks`,
-  ANNOUNCEMENT: `/api/announcement`,
 };
