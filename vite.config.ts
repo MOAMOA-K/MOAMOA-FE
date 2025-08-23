06233daa20ab4d316d982d1425a558e7908a9fb0
Junsub Lee <lee980605@knu.ac.kr>
2025-08-24 04:37:35 +0900

    chore: 카카오 맵 및 서버 연결 위해서 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // 포트 5178로 변경 -> 임시로 서버 연결위해 사용
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
