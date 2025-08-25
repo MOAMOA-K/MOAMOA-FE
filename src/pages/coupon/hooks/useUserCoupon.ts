fc823a394644953db88c83bc5ee023b99eee8a0c
Jeong In Jun <jijun897@naver.com>
2025-08-25 17:59:12 +0900

    feat: 쿠폰 교환 후 유저 데이터 다시 받아와 포인트 업데이트되도록 구현

--- src/pages/coupon/hooks/useUserCoupon.ts
+++ src/pages/coupon/hooks/useUserCoupon.ts
@@ -2,11 +2,17 @@ import {
   postUserCoupon,
   type PostUserCouponParams,
 } from '@/api/user-coupon/postUserCoupon';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const useUserCoupon = () => {
+  const queryClient = useQueryClient();
   const { mutate } = useMutation<void, Error, PostUserCouponParams>({
     mutationFn: postUserCoupon,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user'],
+      });
+    },
   });
 
   return { postUserCoupon: mutate };
